cf0714520f826360d9f3ae23dbf8e7992f28452f
mites-vasoya <78782844+mites-vasoya@users.noreply.github.com>
2024-02-18 19:47:16 +0530

    vercel analytics and insights added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
+ 
+inject();
+injectSpeedInsights();
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
